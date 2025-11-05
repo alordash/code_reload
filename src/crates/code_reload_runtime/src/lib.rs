88603392a623bc6ff87056cb223decf697789c5a
aorazov <aleksei.orazov@devexpress.com>
2025-11-05 17:08:03 +0400

    global hotreload variable

--- src/crates/code_reload_runtime/src/lib.rs
+++ src/crates/code_reload_runtime/src/lib.rs
@@ -1,9 +1,11 @@
 mod constants;
+mod global_hotreload_variable_macro;
 mod hotreload_library;
 mod hotreload_payload;
 mod hotreload_watch;
 mod library_id;
 mod library_wrapper;
+
 pub use constants::*;
 pub use hotreload_library::*;
 pub use hotreload_payload::*;
