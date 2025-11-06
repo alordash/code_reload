da34018297eb5d4b333bafc3f1bec5f0549e5d1a
aorazov <aleksei.orazov@devexpress.com>
2025-11-06 17:47:15 +0400

    Testing

--- src/crates/test_library/bin/run.rs
+++ src/crates/test_library/bin/run.rs
@@ -4,8 +4,8 @@ fn main() {
     let mut i = 0;
     loop {
         i += 1;
-        let a = 44;
-        let b = 18;
+        let a = 18;
+        let b = 44;
         let result = add(a, b);
         println!("#{i}. {a} + {b} = {result}");
         std::thread::sleep(std::time::Duration::from_millis(500));
--- src/crates/test_library/src/lib.rs
+++ src/crates/test_library/src/lib.rs
@@ -4,5 +4,10 @@ include!(concat!(env!("OUT_DIR"), "/__code_reload_hotreload.rs"));
 
 #[code_reload::hotreload]
 pub fn add(left: i32, right: i32) -> i32 {
-    left + right * 10
+    // return left - right;
+    let mut a = 0;
+    for i in left..right {
+        a += i;
+    }
+    a + left + right * 10
 }
