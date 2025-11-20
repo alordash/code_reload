adfd06ff4775bd4a28bd27245bf93c3ea6bc58ea
alordash <alordash1@gmail.com>
2025-11-20 16:56:45 +0400

    Fixed syntax extraction

--- src/crates/code_reload/src/lib.rs
+++ src/crates/code_reload/src/lib.rs
@@ -4,7 +4,7 @@ pub use code_reload_core::LibraryWrapper;
 
 pub mod runtime {
     pub use code_reload_build::runtime::{
-        HotreloadLibrary, IHotreloadPayload, LockedHotreloadLibrary,
+        HotreloadLibrary, IHotreloadPayload, LockedHotreloadLibrary, start_hotreload_watch,
     };
     pub use code_reload_build::start_hotreload_watchers;
 }
--- src/crates/code_reload_core/src/constants.rs
+++ src/crates/code_reload_core/src/constants.rs
@@ -1,4 +1,5 @@
 pub const RUNTIME_TARGET_KEYWORD: &'static str = "runtime";
+
 pub const GENERATED_STATIC_HOTRELOAD_VARIABLE_NAME: &'static str = "HOTRELOAD";
 // TODO - add test to verify that `GENERATED_CODE_FILE_NAME` starts with `GENERATED_CODE_PREFIX`
 pub const GENERATED_CODE_PREFIX: &'static str = "__code_reload";
