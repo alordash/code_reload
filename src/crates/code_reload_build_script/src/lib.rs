77803c5dd7909066d3c59d5992f490597aa3d227
alordash <alordash1@gmail.com>
2025-11-20 16:56:43 +0400

    Adding build script

--- src/crates/code_reload_build_script/src/lib.rs
+++ src/crates/code_reload_build_script/src/lib.rs
@@ -1,14 +1,25 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use std::path::{Path, PathBuf};
+
+pub fn get_all_rust_file_paths(path: &Path) -> Vec<PathBuf> {
+    let mut rust_file_paths = Vec::new();
+    recursive_get_all_rust_file_paths(path, &mut rust_file_paths);
+    return rust_file_paths;
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+const RUST_FILE_EXTENSION: &'static str = "rs";
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+fn recursive_get_all_rust_file_paths(path: &Path, rust_file_paths: &mut Vec<PathBuf>) {
+    for dir_entry in std::fs::read_dir(path).expect("Should be able to read all dir") {
+        let inner_path = dir_entry.unwrap().path();
+        if inner_path.is_file() {
+            if inner_path
+                .extension()
+                .is_some_and(|ext| ext == RUST_FILE_EXTENSION)
+            {
+                rust_file_paths.push(inner_path);
+            }
+        } else {
+            recursive_get_all_rust_file_paths(&inner_path, rust_file_paths);
+        }
     }
 }
